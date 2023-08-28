31b5444486be36a89825436b6c877c041f4d62eb
MichałM <michalhm@gmail.com>
2023-08-28 21:15:02 +0200

    Stable, download data from API

--- main.py
+++ main.py
@@ -4,12 +4,35 @@ import requests
 import json
 from functions import select_location, get_date
 
+URL = "https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=rain&daily=rain_sum&timezone=Europe%2FLondon&start_date={searched_date}&end_date={searched_date}"
+
 
 class WeatherForecaster:
-    def __init__(self):
-        self.load = self.load_data()
-        self.save = self.save_data()
-        self.request = self.api_request()
+    def __init__(self) -> None:
+        pass
+
+    def request(self, latitude, longitude, searched_date, url):
+        api_data = requests.get(url.format(
+            latitude=latitude, longitude=longitude, searched_date=searched_date))
+        if api_data.status_code == 200:
+            api_data = json.loads(api_data.text)
+        else:
+            print("Bład wczytywania danych.")
+            return False
+        if not api_data:
+            pass
+        else:
+            rain_sum = api_data["daily"].get("rain_sum")[0]
+            try:
+                rain_sum = float(rain_sum)
+                if rain_sum > 0:
+                    print("Będzie padać")
+                elif rain_sum == 0:
+                    print("Nie będzie padać")
+                else:
+                    print("Nie wiem")
+            except ValueError:
+                print("Nie wiem")
 
     def __setitem__():
         pass
@@ -24,3 +47,5 @@ class WeatherForecaster:
 # Run App
 latitude, longitude = select_location()
 searched_date = get_date()
+weather_forecaste = WeatherForecaster()
+weather_forecaste.request(latitude, longitude, searched_date, URL)
