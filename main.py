8ee9246aceedbfc6ddefc773a33e1368327197b8
MichałM <michalhm@gmail.com>
2023-08-28 19:42:35 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Previous Code Version available: https://github.com/Mistermichu/Python6
+
+import requests
+import json
+from datetime import datetime, timedelta
+
+class WeatherForecaster:
+    def __init__(self):
+        self.load = self.load_data()
+        self.save = self.save_data()
+        self.request = self.api_request()
+
+    def __setitem__ ():
+        pass
+
+    def __getitem__():
+        pass
+
